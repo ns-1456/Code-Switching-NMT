e7b55a6b9376e363b9a94c301e96b219721d28b0
NS <ns-1456@users.noreply.github.com>
2025-12-16 16:17:45 -0500

    Add training dataset and collate

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -29,3 +29,12 @@ EMOJI_RE = re.compile(
     "["
     "\U0001F600-\U0001F64F"  # emoticons
     "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F680-\U0001F6FF"  # transport & map
+    "\U0001F1E0-\U0001F1FF"  # flags
+    "\U00002702-\U000027B0"
+    "\U000024C2-\U0001F251"
+    "\U0001F900-\U0001F9FF"  # supplemental symbols
+    "\U0001FA00-\U0001FA6F"  # chess symbols
+    "\U0001FA70-\U0001FAFF"  # symbols extended-A
+    "]+",
+    flags=re.UNICODE,
